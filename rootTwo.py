17c82998894d52fab762c1a4a061b1948cb5bd26
Umer <57160083+u7javed@users.noreply.github.com>
2020-01-10 16:18:21 -0500

    Create rootTwo.py

---
+++ rootTwo.py
@@ -0,0 +1,22 @@
+import math
+import sys
+
+def main():
+    quit = True;
+    while quit:
+        num = input("enter a number")
+        if num == "q" or num == "Q":
+            sys.exit()
+        num = float(num)
+        print(newtonsMethod(num, 20))
+
+def newtonsMethod(a, iter):
+    x = a
+    for i in range(iter):
+        f = x*x - a
+        fDerivative = 2*x
+        x = x - f/fDerivative
+    return x
+
+main()
+
